e6021b8ab87ed005e326121a809331a0fe88b91d
Evgeniya <103037678+EvgenyaBond@users.noreply.github.com>
2022-04-11 23:01:14 +0300

    Add files via upload

---
+++ Postman_autotest.js
@@ -0,0 +1,12 @@
+
+pm.test("планета", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Response time is less than 200ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(200);
+});
+
+pm.test("Status code name has string", function () {
+    pm.response.to.have.status("Created");
+});
